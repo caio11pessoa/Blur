11f79163feecddfc09c87871244d84b983e55af5
CaioPessoa <caiopessoa1298@gmail.com>
2025-03-11 15:05:54 -0300

    Initial Commit

---
+++ BlurBackground/BlurBackgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlurBackgroundApp.swift
+//  BlurBackground
+//
+//  Created by Caio de Almeida Pessoa on 11/03/25.
+//
+
+import SwiftUI
+
+@main
+struct BlurBackgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlurBackground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlurBackground
+//
+//  Created by Caio de Almeida Pessoa on 11/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
